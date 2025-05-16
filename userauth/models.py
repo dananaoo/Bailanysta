c885a2ba8fc505c195c28408a3c27dc96ac5d33d
dananaoo <147193892+dananaoo@users.noreply.github.com>
2025-05-16 19:04:49 +0500

    Update models.py

--- userauth/models.py
+++ userauth/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 User = get_user_model()
 
-# Create your models here.
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField(primary_key=True, default=0)
